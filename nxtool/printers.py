8a531ca53dd6a9ab2e4201a4a04b5d9b3425ce8c
Julien (jvoisin) Voisin <jvo+github@nbs-system.com>
2016-08-26 15:38:12 +0200

    Improve a bit some colours

--- nxtool/printers.py
+++ nxtool/printers.py
@@ -5,8 +5,8 @@ Helper functions to display data on stdout
 
 def print_statistics(results):
     for key, value in results.items():
-        print('# \033[95m%s\033[0m #' % key.upper())
-        print('\n'.join('\033[91m%s\033[0m: %s' % (i, j) for i, j in value.items()) + '\n')
+        print('# \033[100m%s\033[0m #' % key.upper())
+        print('\n'.join('\033[32m%s\033[0m: \033[1m%s\033[0m' % (i, j) for i, j in value.items()) + '\n')
 
 
 def print_generic(results):
