fcb643556bb679c39d7ce5ebb6e80fa6b1e52e4a
Julien (jvoisin) Voisin <jvo+github@nbs-system.com>
2016-08-26 17:55:30 +0200

    Add some more doc

--- nxtool/whitelists_generators/cookies.py
+++ nxtool/whitelists_generators/cookies.py
@@ -12,7 +12,10 @@ def generate_whitelist(provider, whitelists):
     """
     logging.info('Generating \033[1mcookies\033[0m rules')
     provider.add_filters({'zone': 'HEADERS', 'var_name': 'cookie'})
-    #data = provider.get_relevant_ids(['uri', 'ip'])
+
+    # We don't require the hits to be spread across users, because the cookie might be only available
+    # to admins for example. But since the browser sends it on every request, it should be
+    # spread amongst urls.
     data = provider.get_relevant_ids(['uri'])
 
     # Filter already whitelisted things
