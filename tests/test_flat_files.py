27c27b9913f7bd3eddc99d86557bfd215c7e9ee1
Julien (jvoisin) Voisin <jvo+github@nbs-system.com>
2016-08-30 18:06:42 +0200

    Improve a bit the test

--- tests/test_flat_files.py
+++ tests/test_flat_files.py
@@ -20,6 +20,23 @@ class TestParseLog(unittest.TestCase):
             images_1002.generate_whitelist(parser, []),
             [{'mz': ['$URL_X:^/phpMyAdmin-2.8.2/scripts/setup.php|URL'], 'wl': [1002], 'msg': 'Images size (0x)'}]
         )
-
         self.assertEqual(images_1002.generate_whitelist(parser, [{'wl': [1002]}]), [])
-        
+
+
+class TestFiltering(unittest.TestCase):
+    maxDiff = None
+    def test_filter_str(self):
+        parser = flat_file.FlatFile('./tests/data/cookies.txt')
+        self.assertEquals([i for i in parser.get_results()][0], {'block': '0',
+           'cscore0': '$UWA', 'id0': '42000227', 'ip': 'X.X.X.X', 'learning': '0', 'score0': '8',
+           'server': 'Y.Y.Y.Y', 'total_blocked': '204', 'total_processed': '472',
+           'uri': '/phpMyAdmin-2.8.2/scripts/setup.php', 'var_name0': 'cookie', 'vers': '0.52', 'zone0': 'HEADERS'}
+        )
+
+    def test_filter_list(self):
+        parser = flat_file.FlatFile('./tests/data/cookies.txt')
+        parser.add_filters({'ip': ['X.X.X.X', 'A.A.A.A']})
+        self.assertEquals([i for i in parser.get_results()], [{'block': '0',
+           'cscore0': '$UWA', 'id0': '42000227', 'ip': 'X.X.X.X', 'learning': '0', 'score0': '8',
+           'server': 'Y.Y.Y.Y', 'total_blocked': '204', 'total_processed': '472',
+           'uri': '/phpMyAdmin-2.8.2/scripts/setup.php', 'var_name0': 'cookie', 'vers': '0.52', 'zone0': 'HEADERS'}])
