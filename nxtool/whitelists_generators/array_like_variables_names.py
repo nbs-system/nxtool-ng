6f45c14610ad8f709452742707cb503cf02f24bd
Julien (jvoisin) Voisin <jvo+github@nbs-system.com>
2016-10-07 13:21:14 +0200

    Fix a typo in array-like variables names logging

--- nxtool/whitelists_generators/array_like_variables_names.py
+++ nxtool/whitelists_generators/array_like_variables_names.py
@@ -42,7 +42,7 @@ def generate_whitelist(provider, whitelists):
 
         stripped_name = __check_and_strip_brackets(var_name)
         if not stripped_name:
-            logging.debug('The variable \033[32m%s\033[0m does not have an expected form', var_name, nb)
+            logging.debug('The variable \033[32m%s\033[0m does not have an expected form', var_name)
             continue
         ret.append({
             'mz': ['$BODY_VAR_X:%s\[.+\]' % stripped_name, '$ARGS_VAR_X:%s\[.+\]' % stripped_name],
