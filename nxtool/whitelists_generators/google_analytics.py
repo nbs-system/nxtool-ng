24cc2560478e370bf88dc1eb5d0254253af33fa9
jvoisin <jvo+github@nbs-system.com>
2016-11-24 12:50:51 +0100

    Simplify the handling of Google Analytics

--- nxtool/whitelists_generators/google_analytics.py
+++ nxtool/whitelists_generators/google_analytics.py
@@ -6,20 +6,20 @@ from . import modify_search
 @modify_search
 def generate_whitelist(provider, whitelists):
     """
-
+    Generate whitelists for Google Analytics cookies
     :param provider:
-    :return:
+    :param list of dict whitelists:
+    :return list of dict:
     """
     logging.info('Generating \033[1mGoogle analytics\033[0m rules')
-    cookies_names = ['__utma', '__utmb', '__utmc', '__utmt', '__utmv', '__utmz']
-    provider.add_filters({'zone': 'ARGS', 'var_name': cookies_names})
-    data = provider.get_relevant_ids(['uri', 'ip'])
 
-    # Filter already whitelisted things
-    already_whitelisted_id = set()
-    for r in whitelists:
-        if 'mz' not in r or any(cookie_name in r['mz'] for cookie_name in cookies_names):
-            already_whitelisted_id = already_whitelisted_id.union(r['wl'])
-    wid = [wid for wid in data if wid not in already_whitelisted_id]
+    for rule in whitelists:
+        provider.add_filters({'id': rule.get('wl', '*'), 'mz': rule.get('mz', '*')}, negative=True)
 
-    return list() if not wid else [{'mz': ['$ARGS_VAR_X:__utm[abctvz]'], 'wl': wid, 'msg': 'Google analytics'}, ]
+    provider.add_filters({'zone': 'ARGS', 'var_name': ['__utma', '__utmb', '__utmc', '__utmt', '__utmv', '__utmz']})
+    ids = provider.get_relevant_ids(['uri', 'ip'])
+
+    ret = list()
+    if ids:
+        ret.append({'mz': ['$ARGS_VAR_X:__utm[abctvz]'], 'wl': ids, 'msg': 'Google analytics'})
+    return ret
