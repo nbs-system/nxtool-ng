8d0cfca3a94e09cf48aedc957cb8b1d07005f2b0
jvoisin <jvo@nbs-system.com>
2017-02-20 11:54:34 +0100

    Remove two useless lines of code

--- nxtool/whitelists_generators/__init__.py
+++ nxtool/whitelists_generators/__init__.py
@@ -1,7 +1,5 @@
 def modify_search(func):
     def wrapper(provider, wl):
-        if not hasattr(provider, 'search'):
-            return func(provider, wl)
         search = provider.search
         ret = func(provider, wl)
         provider.search = search
